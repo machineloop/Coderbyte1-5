ed25baeefa0250df06ee9dff61a0f09765107f7d
J. Andrew Brassington <jabbrass@zoho.com>
2014-05-06 14:43:58 -0500

    Created project

---
+++ OurFirstVSProject/JavaScript.js
@@ -0,0 +1,16 @@
+﻿function LetterChanges(str) {
+    var alphabetArray = "abcdefghijklmnopqrstuvwxyza".split("");
+    var strArray = str.split("");
+    for (var i = 0; i < strArray.length; i = i + 1) {
+        var strIndex = alphabetArray.indexOf(strArray[i]);
+        if (strIndex !== -1) {
+            strArray[i] = alphabetArray[strIndex + 1];
+        }
+    }
+    return strArray.join("");
+}
+
+var result = LetterChanges("hello*3");
+alert(result);
+
+//should get "Ifmmp*3" as a result
